082168c7ff3a05e4aa3c38aa52df937f360405fe
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-12-25 13:13:56 +0530

    Updated Backend Server Url

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetch Data From The Server -> Bot's Response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://ai-codewizard.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
