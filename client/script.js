e57d4a50741bc4f74f520912a57ac8fa4e34562e
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-03-15 17:56:02 +0530

    Bug bix

--- client/script.js
+++ client/script.js
@@ -72,8 +72,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetch Data From The Server -> Bot's Response
-  // const response = await fetch("https://ai-codewizard.onrender.com/", {
-  const response = await fetch("http://localhost:5000/geminiapi", {
+  const response = await fetch("https://ai-codewizard.onrender.com/geminiapi", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
